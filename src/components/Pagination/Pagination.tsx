1ae91f812bfe20fe7a0320c7ddf0df7eef656385
min <hororonoodle@gmail.com>
2022-03-08 02:31:52 +0900

    feat : 로직 저장

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -14,8 +14,28 @@ type Data = {
   category_names: string[];
 };
 
+type RegionsData = {
+  product_code: number;
+  region_id: number;
+  image_url: string;
+  gender: string;
+  attributes: [RegionsAttr];
+  category_names: string[];
+};
+
+interface RegionsAttr {
+  style: string;
+  season: string;
+  occasion: string;
+  fabric: string;
+  sense: string;
+  pattern: string;
+}
+
 const Pagination = () => {
   const [posts, setPosts] = useState<Data[]>([]);
+  const [regionsData, setRegionsData] = useState<RegionsData[]>([]);
+
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(70);
@@ -24,31 +44,96 @@ const Pagination = () => {
   const option = searchParams.get('option');
   const target = searchParams.get('target');
 
-  useEffect(() => {
-    const fetchKeywordData = async (target: string) => {
-      setLoading(true);
-      try {
-        const { data } = await axios.get('https://static.pxl.ai/problem/data/products.json');
-        // data.map((value,idx) =>)
-        setPosts(data);
-        setLoading(false);
-      } catch (err) {
-        console.log(err);
-        setLoading(false);
+  const fetchKeywordData = async (target: string, stat: string) => {
+    setLoading(true);
+    const filteredArray: Data[] = [];
+    try {
+      const { data } = await axios.get('https://static.pxl.ai/problem/data/products.json');
+      console.log(data);
+      switch (stat) {
+        case 'keyword': {
+          data.map((value: Data) => {
+            if (value.name.includes(target)) {
+              filteredArray.push(value);
+            }
+          });
+          break;
+        }
+        case 'img': {
+          let itemName = '';
+          data.map((value: Data) => {
+            if (value.image_url === target) {
+              // axios
+              //   .get('https://static.pxl.ai/problem/data/products.json')
+              //   .then(({ data }) =>
+              //     data.map((val: Data) => {
+              //       if (val.name.includes(target)) {
+              //         filteredArray.push(val);
+              //       }
+              //     }),
+              //   )
+              //   .catch();
+              itemName = value.name;
+              return;
+            }
+          });
+          data.map((value: Data) => {
+            if (value.name.includes(itemName)) {
+              filteredArray.push(value);
+            }
+          });
+          console.log(filteredArray);
+          break;
+        }
       }
-    };
 
+      setPosts(filteredArray);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  const fetchCodeData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get('https://static.pxl.ai/problem/data/regions.json');
+      setRegionsData(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     if (option) {
       switch (option) {
         case 'keyword':
-          console.log(option + 'keyword!');
+          console.log(option + ' keyword!');
           if (target) {
-            fetchKeywordData(target);
+            fetchKeywordData(target, 'keyword');
           }
-
           break;
         case 'code':
-          console.log(option + 'keyword!');
+          console.log(option + ' code!');
+          if (target) {
+            if (target.includes('https://static.pxl.ai/problem/images')) {
+              console.log('이미지 ' + target);
+
+              fetchCodeData();
+              regionsData.map((value: RegionsData) => {
+                if (value.image_url === target) {
+                  fetchKeywordData(target, 'img');
+                }
+              });
+            } else if (!isNaN(Number(target))) {
+              console.log('숫자임 ' + target);
+            } else {
+              console.log('키워드임 ' + target);
+            }
+          }
           break;
         default:
           console.log('err');
