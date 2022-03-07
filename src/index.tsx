82ec20c528ce84c7d90dc1be64d30229ad995aa3
jinn2u <bsybear623@gmail.com>
2022-03-07 23:19:48 +0900

    feat: globalStyle 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './theme/GlobalStyles';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
+      <GlobalStyle />
       <App />
     </React.StrictMode>
   </BrowserRouter>,
