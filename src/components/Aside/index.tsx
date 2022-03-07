c5114fce95f83106c5566a52c09da4072c0f798f
jinn2u <bsybear623@gmail.com>
2022-03-08 00:26:10 +0900

    feat: 마크업

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,7 +1,39 @@
+import { filterCategoryC1 } from './utils/filterCategoryC1';
+import { useGetAsideItem } from './utils/useGetAsideItem';
+import * as S from './style';
+import { transAttribute } from './utils/transAttribute';
 interface Aside {
   asideKey: string;
 }
 const Aside = ({ asideKey }: Aside) => {
-  return <div>{asideKey}</div>;
+  const [data, isLoading] = useGetAsideItem(asideKey);
+
+  return (
+    <S.Wrapper>
+      {isLoading && '로딩 중입니다.'}
+      {!isLoading && !data && '데이터가 존재하지 않습니다.'}
+      {!isLoading && data && (
+        <>
+          <S.Img src={data.image_url} />
+          <S.H1>ITEMS</S.H1>
+          <S.CateUl>
+            {filterCategoryC1(data.category_names).map((v, idx) => (
+              <S.CateLi key={idx}>{v}</S.CateLi>
+            ))}
+          </S.CateUl>
+          <S.Hr />
+          <S.H1>Attributes</S.H1>
+          <S.AttrUl>
+            {transAttribute(data).map(([key, value], idx) => (
+              <S.AttrLi key={idx}>
+                <S.AttrKey>{`#${key}`}</S.AttrKey>
+                <S.AttrVal>{value}</S.AttrVal>
+              </S.AttrLi>
+            ))}
+          </S.AttrUl>
+        </>
+      )}
+    </S.Wrapper>
+  );
 };
 export default Aside;
---
+++ src/components/Aside/style.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  border: 1px solid red;
+  gap: 20px;
+  width: 320px;
+  box-sizing: border-box;
+`;
+export const Img = styled.img`
+  object-fit: cover;
+  width: inherit;
+`;
+export const H1 = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const CateUl = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+export const CateLi = styled.li`
+  display: inline-block;
+  background-color: #bb78ff;
+  color: white;
+  font-size: 16px;
+  padding: 4px 8px;
+`;
+export const Hr = styled.hr`
+  margin: 10px 0px;
+  border-top: none;
+  border-bottom: 0.5px solid lightgray;
+`;
+
+export const AttrLi = styled.li`
+  display: flex;
+  flex-direction: column;
+`;
+export const AttrUl = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+export const AttrKey = styled.h2`
+  font-size: 16px;
+  font-weight: bold;
+  color: #bb78ff;
+`;
+
+export const AttrVal = styled.h3`
+  font-size: 14px;
+`;
