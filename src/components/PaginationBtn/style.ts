ba98ab7f0ab3bfe39b9090d87aaba15d657e088a
Parksangwoo <psu8430@gmail.com>
2022-03-08 10:16:43 +0900

    Feat: 페이지네이션 추가

--- src/components/PaginationBtn/style.ts
+++ src/components/PaginationBtn/style.ts
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
 
 export const PageListWrap = styled.ul`
-  width: 500px;
   list-style: none;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
+
+export const PagiButton = styled.button<{ isTarget: boolean }>`
+  margin: 20px 0;
+  width: 40px;
+  height: 40px;
+  font-size: 24px;
+  font-weight: bold;
+  color: ${(props) => (props.isTarget ? '#8E4FF8' : '#d7c0ff')};
+
+  * {
+    margin: auto;
+  }
 `;
