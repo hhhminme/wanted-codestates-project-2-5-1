6b320872ed8357061a0db25bc31153f200c6951b
Parksangwoo <psu8430@gmail.com>
2022-03-08 14:11:53 +0900

    Style: 로딩 컴포넌트 수정

--- src/components/Skeleton/Item/style.ts
+++ src/components/Skeleton/Item/style.ts
@@ -4,8 +4,8 @@ import { Item } from '.';
 export const Content = styled.div<Item>`
   display: inline-block;
   border-radius: ${({ type }) => (type === 'round' ? '50%' : '10px')};
-  width: ${({ width }) => `${width}px`};
-  height: ${({ height }) => `${height}px`};
+  width: ${({ width }) => (typeof width === 'number' ? `${width}px` : width)};
+  height: ${({ height }) => (typeof height === 'number' ? `${height}px` : height)};
   background-image: linear-gradient(90deg, #dfe3e8 0px, #efefef 40px, #dfe3e8 80px);
   background-size: 200% 100%;
   background-position: 0 center;
