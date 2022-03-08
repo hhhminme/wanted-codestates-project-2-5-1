32dcc4dc47fc8c8cb2cdd7e05a9087757e077619
Parksangwoo <psu8430@gmail.com>
2022-03-08 14:12:34 +0900

    Style: 스타일 수정

--- src/components/SearchBar/style.ts
+++ src/components/SearchBar/style.ts
@@ -1,12 +1,12 @@
-import style from 'styled-components';
+import styled from 'styled-components';
 
-export const SearchBarWrapper = style.div`
-  display: flex; 
+export const SearchBarWrapper = styled.div`
+  display: flex;
   flex-direction: column;
-  max-width: 700px;
-  width: 80vw;
+  min-width: 300px;
+  width: 40vw;
 `;
-export const SearchBar = style.div`
+export const SearchBar = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
@@ -16,7 +16,7 @@ export const SearchBar = style.div`
   box-shadow: 0px 0px 15px 0px #c8c8c8;
 `;
 
-export const SearchBarInput = style.input`
+export const SearchBarInput = styled.input`
   width: calc(100% - 75px);
   height: 40px;
   font-size: 14px;
@@ -29,9 +29,9 @@ export const SearchBarInput = style.input`
   }
 `;
 
-export const SearchBarButton = style.button`
+export const SearchBarButton = styled.button`
   width: 75px;
-  background: #8E4FF8;
+  background: #8e4ff8;
   color: white;
   border-radius: 0 20px 20px 0;
   border: none;
@@ -42,7 +42,7 @@ export const SearchBarButton = style.button`
   }
 `;
 
-export const SearchBarOption = style.div`
+export const SearchBarOption = styled.div`
   display: flex;
   align-items: center;
   font-size: 14px;
@@ -50,10 +50,52 @@ export const SearchBarOption = style.div`
   label {
     margin: 0 10px 0 5px;
   }
-  &>*:hover{
+  & > *:hover {
     cursor: pointer;
+  }
+
+  @media screen and (max-width: 550px) {
+    justify-content: center;
   }
 `;
 
-export const RadioInput = style.input`
+export const RadioInput = styled.input.attrs({ type: 'radio' })`
+  --active: #fff;
+  --active-inner: #8e4ff8;
+  --border: #8e4ff8;
+  --background: #fff;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  height: 15px;
+  position: relative;
+  cursor: pointer;
+  border: 1px solid var(--border);
+  background: var(--b, var(--background));
+  border-radius: 50%;
+  &:after {
+    content: '';
+    display: block;
+    left: 0;
+    top: 0;
+    position: absolute;
+  }
+  &:not(.switch) {
+    width: 15px;
+    &:after {
+      opacity: var(--o, 0);
+    }
+    &:checked {
+      --o: 1;
+    }
+  }
+  &:after {
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: var(--active-inner);
+    transform: scale(var(--s, 0.7));
+  }
+  &:checked {
+    --s: 0.7;
+  }
 `;
